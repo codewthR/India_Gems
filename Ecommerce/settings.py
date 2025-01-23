e77794c036a111c8156c6bbe347b3be60fac950e
codewthR <rahulbhatia.ramm@gmail.com>
2025-01-23 11:02:23 +0530

    Database Edited

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -76,17 +76,23 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'Ecomm_cart',
-		'USER': 'root',
-		'PASSWORD': '1234',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
-}
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'Ecomm_cart',
+# 		'USER': 'root',
+# 		'PASSWORD': '1234',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 	}
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -5,11 +5,11 @@ from Details_all.models import state_about
 def disp(request):
     if request.method == "GET":
         all_data = state_about.objects.all()
-        one = state_about.objects.get(id=32)
-        two = state_about.objects.get(id=5)
+        # one = state_about.objects.get(id=32)
+        # two = state_about.objects.get(id=5)
         context={
             'all_data':all_data,
-            'one' : one,
-            'two':two,
+            # 'one' : one,
+            # 'two':two,
         }
-    return render(request, 'index.html' ,context)
+    return render(request, 'index.html', context )
